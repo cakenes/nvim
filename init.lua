2f72c0e97844e950aac10225a3f14cd7444b8776
cakenes <jyri.kurtakko@gmail.com>
2026-03-17 08:50:47 +0200

    Automaticly close command-line window

--- init.lua
+++ init.lua
@@ -8,18 +8,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- vim.api.nvim_create_autocmd("VimEnter", {
---     callback = function()
---         vim.defer_fn(function()
---             vim.cmd("Neotree show git_status left")
---             vim.cmd("belowright split | Neotree show filesystem")
---
---             vim.defer_fn(function()
---                 vim.cmd("wincmd l")
---             end, 100)
---         end, 100)
---     end,
--- })
+vim.api.nvim_create_autocmd("CmdwinEnter", {
+    callback = function()
+        vim.cmd("quit")
+    end,
+})
 
 require("config.keymap")
 require("config.options")
