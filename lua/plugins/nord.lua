dcaf32c5a7085c0f9a81257cc04538cd0f2f46ac
cakenes <jyri.kurtakko@gmail.com>
2026-04-22 08:47:19 +0300

    Disable vscode add nord

---
+++ lua/plugins/nord.lua
@@ -0,0 +1,57 @@
+return {
+    "shaunsingh/nord.nvim",
+    config = function()
+        vim.g.nord_italic = true
+        vim.g.nord_borders = true
+        vim.g.nord_disable_background = false
+
+        require("nord").set()
+
+        local colors = require("nord.colors")
+
+        -- Rainbow Delimiters (Using Nord's vibrant palette)
+        vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = colors.nord11 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = colors.nord13 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = colors.nord9 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = colors.nord12 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = colors.nord14 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = colors.nord15 })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = colors.nord8 })
+
+        -- Notification and UI Components
+        -- Nord0 is the darkest background, Nord3 is a brighter gray/Polar Night
+        vim.api.nvim_set_hl(0, "NotifyBackground", { bg = colors.nord0 })
+
+        -- Diagnostics (Using Nord3/Nord4 for subtle hints)
+        vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = colors.nord10, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = colors.nord4, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderLineInfo", { sp = colors.nord10, undercurl = true })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderLineHint", { sp = colors.nord4, undercurl = true })
+
+        -- NeoTree and Selection
+        vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = colors.nord1 })
+        vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = colors.nord8, bg = "NONE", bold = true })
+
+        -- GitGutter / Gitsigns Line Highlights
+        vim.api.nvim_set_hl(0, "GitSingsDeleteLn", { bg = colors.nord11, blend = 20 })
+        vim.api.nvim_set_hl(0, "GitSingsChangeLn", { bg = colors.nord13, blend = 20 })
+        vim.api.nvim_set_hl(0, "GitSignsAddLn", { bg = colors.nord14, blend = 20 })
+
+        -- Borders
+        vim.api.nvim_set_hl(0, "LazyGitBorder", { fg = colors.nord3, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { fg = colors.nord3, bg = "NONE" })
+
+        -- Git Blame (Subtle gray)
+        vim.api.nvim_set_hl(0, "GitBlame", { fg = colors.nord3, bg = colors.nord0 })
+
+        -- Scrollbar
+        vim.api.nvim_set_hl(0, "ScrollbarHandle", { bg = colors.nord3, fg = "NONE" })
+        vim.api.nvim_set_hl(0, "ScrollbarSearch", { bg = colors.nord0, fg = colors.nord14 })
+        vim.api.nvim_set_hl(0, "ScrollbarSearchHandle", { bg = colors.nord3, fg = colors.nord14 })
+
+        -- Search Lens (Hiding/Blending)
+        vim.api.nvim_set_hl(0, "HlSearchLens", { fg = colors.nord3, bg = "NONE", blend = 100 })
+        vim.api.nvim_set_hl(0, "HlSearchLensNear", { fg = colors.nord3, bg = "NONE", blend = 100 })
+        vim.api.nvim_set_hl(0, "HlSearchLensCurrent", { fg = colors.nord3, bg = "NONE", blend = 100 })
+    end,
+}
