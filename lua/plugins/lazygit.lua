e3bd57d6bafd679379ceae001ea8d644f1ff4923
cakenes <jyri.kurtakko@gmail.com>
2026-07-12 11:04:05 +0300

    Fix laxygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -10,5 +10,17 @@ return {
     },
     config = function()
         require("telescope").load_extension("lazygit")
+
+        -- Close the lazygit floating window (used remotely by lazygit's
+        -- os.edit/os.open commands so opening a file in the parent Neovim
+        -- instance also closes the lazygit popup).
+        function _G.LazygitCloseFloat()
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local buf = vim.api.nvim_win_get_buf(win)
+                if vim.bo[buf].filetype == "lazygit" then
+                    pcall(vim.api.nvim_win_close, win, true)
+                end
+            end
+        end
     end,
 }
